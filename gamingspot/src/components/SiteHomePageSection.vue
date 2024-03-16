7d27dec444d646a183714b773cb444c9c4f93d3c
dodo6104 <dodo6104@gmail.com>
2024-03-16 21:05:29 +0100

    Add section component and made all section to homepage

---
+++ gamingspot/src/components/SiteHomePageSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="latest-greatest">
+    <h1 class="Sections">{{ sectionName }}</h1>
+      <div class="product-grid">
+        <SiteProduct v-for="product in products" :key="product.id" :product="product"/>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  import SiteProduct from './SiteProduct.vue';
+  
+  export default {
+    name: 'SiteHomePageSection',
+    components: {
+      SiteProduct
+    },
+    props: {
+      products: Array,
+      sectionName: String, 
+    },
+  }
+  </script>
+  
+  <style src="../styles/SiteSectionPage.css"></style>
+  
--- gamingspot/src/views/SiteHome.vue
+++ gamingspot/src/views/SiteHome.vue
@@ -1,35 +1,57 @@
 <template>
-  <div class = "Content">
-  <section class="latest-greatest">
-    <h2 class = "Sections">Latest & Greatest ></h2>
-    <div class="grid-holder">
-    <div class="product-grid">
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-      <SiteProduct/>
-    </div>
-  </div>
-  </section>
-</div>
+    <div class="LatestGreatest"> <Section :products="LatestGreatestProducts" sectionName="Latest & Greatest >"/></div>
+    <div class="Bestsellers"> <Section :products="BestsellersProducts" sectionName="Bestsellers >"/> </div>
+    <div class="MegaDiscounts"> <Section :products="MegaDiscountsProducts" sectionName="Mega Discounts >"/> </div>
 </template>
 
 <script>
 import SiteProduct from '../components/SiteProduct.vue';
+import Section from '../components/SiteHomePageSection.vue';
 export default {
   name: 'SiteHome',
   components: {
-    SiteProduct
+     Section
+  },
+  data() {
+    return {
+      LatestGreatestProducts: [
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+      ],
+      BestsellersProducts: [
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+      ],
+      MegaDiscountsProducts: [
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+        { SiteProduct },
+      ],
+    };
   },
 }
 </script>
 
 <style src="../styles/SiteHomePage.css"></style>
-
