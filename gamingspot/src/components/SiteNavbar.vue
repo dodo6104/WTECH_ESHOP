c96fba78a7783a8e05c74851753ba66475b005a0
dodo6104 <dodo6104@gmail.com>
2024-03-17 01:45:22 +0100

    Navbar search resize not done yet

--- gamingspot/src/components/SiteNavbar.vue
+++ gamingspot/src/components/SiteNavbar.vue
@@ -1,35 +1,28 @@
 <template>
   <nav class="navbar">
-    <!-- Logo or Branding -->
-  <button type="button" @click="$router.push('/')">
-    <div class="navbar-brand">GamingSpot</div>
-  </button>
-
-    <!-- Navigation Links -->
-    <div class="navbar-menu"> 
-      <button>
-      <div class="navbar-item" >Platforms ▽</div>
+    <button type="button" @click="$router.push('/')">
+      <div class="navbar-brand">GamingSpot</div>
     </button>
-    <button>
-      <div class="navbar-item">Genres ▽</div>
-    </button>
+    <div class="navbar-menu">
+      <button v-show="isVisible">
+        <div class="navbar-item" >Platforms ▽</div>
+      </button>
+      <button v-show="isVisible">
+        <div class="navbar-item">Genres ▽</div>
+      </button>
     </div>
-
-    <!-- Search Bar -->
     <div class="navbar-search">
-      <input type="text" placeholder="Search">
-      <button type="button">
-        <img src="../assets/icons/search.png" alt="User" />
+      <input type="text" placeholder="Search" >
+      <button type="button" id="SearchMagnifyingGlass" @click="buttonClicked" :disabled="isButtonDisabled">
+        <img src="../assets/icons/search.png" alt="Search" />
       </button>
     </div>
-
-    <!-- Icons or Buttons -->
     <div class="navbar-icons">
-      <button type="button"   @click="$router.push('/login')">
-        <img src="../assets/icons/user.png" alt="User" />
+      <button type="button" @click="$router.push('/login')">
+        <img src="../assets/icons/user.png" alt="Login" />
       </button>
       <button type="button">
-        <img src="../assets/icons/shopping-cart.png" alt="User" />
+        <img src="../assets/icons/shopping-cart.png" alt="Cart" />
       </button>
     </div>
   </nav>
@@ -38,8 +31,29 @@
 <script>
 export default {
   name: 'SiteNavbar',
-  
-}
+  data() {
+    return {
+      isButtonDisabled: true,
+      isVisible: true,
+    };
+    
+  },
+  methods: {
+    buttonClicked() {
+      this.isVisible = !this.isVisible;
+    },
+    checkScreenWidthAndAdjustButton() {
+      this.isButtonDisabled = window.innerWidth >= 1052+22;
+    },
+  },
+  mounted() {
+    this.checkScreenWidthAndAdjustButton();
+    window.addEventListener('resize', this.checkScreenWidthAndAdjustButton);
+  },
+  beforeUnmount() { 
+    window.removeEventListener('resize', this.checkScreenWidthAndAdjustButton);
+  },
+};
 </script>
 
 <style src="../styles/SiteNavbar.css"></style>
