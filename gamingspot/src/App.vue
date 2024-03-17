fb4d719441f2470a73c98cfeee2208422e79906b
dodo6104 <dodo6104@gmail.com>
2024-03-17 20:33:17 +0100

    Platform menu first view added

--- gamingspot/src/App.vue
+++ gamingspot/src/App.vue
@@ -1,22 +1,48 @@
 <template>
   <div id="app">
-    <Navbar />
+    <Navbar @platform-clicked="handlePlatformClick" />
+    <platformMenu v-if="isPlatformVisible" ref="PlatformMenu"/>
     <router-view/>
     <Footer />
   </div>
 </template>
   
-  <script>
-  import Navbar from './components/SiteNavbar.vue'
-  import Footer from './components/SiteFooter.vue'
-  
-  export default {
-    name: 'App',
-    components: {
-      Navbar,
-      Footer
+<script>
+import Navbar from './components/SiteNavbar.vue'
+import Footer from './components/SiteFooter.vue'
+import PlatformMenu from './components/SitePlatformMenu.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+    PlatformMenu,
+  },
+  data() {
+    return {
+      isPlatformVisible: false
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.isPlatformVisible = false);
+  },
+  beforeUnmount() {
+    window.removeEventListener('resize', this.isPlatformVisible = false);
+  },                                                            
+  methods: {
+    handlePlatformClick(centerX) {
+      this.isPlatformVisible = !this.isPlatformVisible
+      this.$nextTick(() => {
+      const PlatformMenu = this.$refs.PlatformMenu?.$el;
+      if (PlatformMenu) {
+        const PlatformMenuRect = PlatformMenu.getBoundingClientRect();
+        PlatformMenu.style.position = 'absolute';
+        PlatformMenu.style.left = `${centerX - PlatformMenuRect.width / 2}px`;
+      }
+    });
     }
   }
-  </script>
+};
+</script>
 
 <style src="./styles/Main.css"></style>
